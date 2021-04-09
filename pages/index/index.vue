ce94df5d47e0a525334a763f12918e9164585fa7
13026132677@163.com <13026132677@163.com>
2021-04-10 01:24:29 +0800

    uni-app练手项目，为爱在欧宝APP做准备

---
+++ pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="page">
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" class="carousel">
+			<swiper-item>
+				<image src="../../static/logo.png" class="carousel"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/tabBar/icon_index.png" class="carousel"></image>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		onLoad() {  
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	@import url("index.css");
+</style>
